197cecfe3e3cc6fcf9a9a358ec87e52d90a62e43
Valentin Latunov <vlatunov@gmail.com>
2024-06-03 22:51:20 +0300

    Disable automatic writing of imgui.ini

--- src/app.cpp
+++ src/app.cpp
@@ -162,18 +162,16 @@ App::~App() {
 void App::run() {
 	if (!initialized) return;
 
+	// Do some setup
 	bool show_demo_window = true;
 	bool show_another_window = false;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 	ImGuiIO& io = ImGui::GetIO();
+	io.IniFilename = nullptr;
 
 	// Main loop
 	while (!glfwWindowShouldClose(glfw.window)) {
 		// Poll and handle events (inputs, window resize, etc.)
-		// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-		// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-		// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-		// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
 		glfwPollEvents();
 
 		// Start the Dear ImGui frame
